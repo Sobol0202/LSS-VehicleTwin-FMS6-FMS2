cfc73962a94d819c5fe584736dbc236557c2e7bc
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2023-06-23 11:27:45 +0200

    Update LSS-VehicleTwin FMS6-FMS2.user.js

--- LSS-VehicleTwin FMS6-FMS2.user.js
+++ LSS-VehicleTwin FMS6-FMS2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS-VehicleTwin FMS6-FMS2
 // @namespace    https://www.leitstellenspiel.de/
-// @version      1.1
+// @version      2.0
 // @description  Schaltet ein Fahrzeug in S6, wenn das andere in S2 geht
 // @author       MissSobol
 // @match        https://www.leitstellenspiel.de/*
@@ -9,37 +9,42 @@
 // ==/UserScript==
 
 // Konfiguration
-const fahrzeug1ID = '51973941'; // Hier Fahrzeug 1 ID eintragen
-const fahrzeug2ID = '46968798'; // Hier Fahrzeug 2 ID eintragen
+const fahrzeugPaare = [
+  {
+    fahrzeug1ID: '51973941', // Hier Fahrzeug 1 ID eintragen
+    fahrzeug2ID: '46968798', // Hier Fahrzeug 2 ID eintragen
+  },
+  {
+    fahrzeug1ID: '15963635', // Hier Fahrzeug 1 ID des nächsten Paares eintragen
+    fahrzeug2ID: '15963639', // Hier Fahrzeug 2 ID des nächsten Paares eintragen
+  },
+  // Weitere Fahrzeugpaare können nach Bedarf hinzugefügt werden
+];
 
 // Status der Fahrzeuge speichern
-let fahrzeug1Status = '0';
-let fahrzeug2Status = '0';
+const fahrzeugStatus = {};
 
 // Funktion zum Überprüfen der Checkboxen der Fahrzeuge
 function checkVehicleCheckboxes() {
-  const checkbox1 = document.getElementById('vehicle_checkbox_' + fahrzeug1ID);
-  const checkbox2 = document.getElementById('vehicle_checkbox_' + fahrzeug2ID);
+  fahrzeugPaare.forEach(fahrzeugPaar => {
+    const checkbox1 = document.getElementById('vehicle_checkbox_' + fahrzeugPaar.fahrzeug1ID);
+    const checkbox2 = document.getElementById('vehicle_checkbox_' + fahrzeugPaar.fahrzeug2ID);
 
-  if (checkbox1 && checkbox2 && checkbox1.checked && checkbox2.checked) {
-    // Beide Checkboxen sind ausgewählt
-//    console.log('Beide Fahrzeug-Checkboxen sind ausgewählt. Korrigiere...');
+    if (checkbox1 && checkbox2 && checkbox1.checked && checkbox2.checked) {
+      // Beide Checkboxen sind ausgewählt
+      checkbox2.checked = false; // Die Checkbox des zweiten Fahrzeugs wird abgewählt
 
-    // Die Checkbox des zweiten Fahrzeugs wird abgewählt
-    checkbox2.checked = false;
-//    console.log(`Fahrzeug ${fahrzeug2ID} Checkbox wurde abgewählt.`);
-
-    // Manuell den "change" Event auslösen, um das System über die Änderung zu informieren
-    const changeEvent = new Event('change', { bubbles: true });
-    checkbox2.dispatchEvent(changeEvent);
-  }
+      // Manuell den "change" Event auslösen, um das System über die Änderung zu informieren
+      const changeEvent = new Event('change', { bubbles: true });
+      checkbox2.dispatchEvent(changeEvent);
+    }
+  });
 }
 
 // Funktion zum Klicken auf den Button basierend auf FMS-Status
 function handleFMSStatus(vehicleID, fmsStatus) {
-  if (fahrzeug1Status === '3' && fahrzeug2Status === '3') {
-//    console.log('Beide Fahrzeuge befinden sich bereits im Status 3. Keine Aktion erforderlich.');
-    return;
+  if (fahrzeugStatus[vehicleID] === '3') {
+    return; // Fahrzeug befindet sich bereits im Status 3, keine Aktion erforderlich
   }
 
   const setFMSURL = `https://www.leitstellenspiel.de/vehicles/${vehicleID}/set_fms/${fmsStatus}`;
@@ -48,9 +53,9 @@ function handleFMSStatus(vehicleID, fmsStatus) {
   xhr.open('GET', setFMSURL, true);
   xhr.onreadystatechange = function() {
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-//      console.log(`Fahrzeug ${vehicleID}: FMS-Status auf ${fmsStatus} umgeschaltet.`);
+      // Fahrzeugstatus erfolgreich aktualisiert
     } else {
-//      console.log(`Fahrzeug ${vehicleID}: Fehler beim Umschalten auf FMS-Status ${fmsStatus}.`);
+      console.log(`Fahrzeug ${vehicleID}: Fehler beim Umschalten auf FMS-Status ${fmsStatus}.`);
     }
   };
   xhr.send();
@@ -61,9 +66,6 @@ document.addEventListener('change', checkVehicleCheckboxes);
 
 // Überwachung der Checkboxen bei Aktualisierungen durch das System
 setInterval(checkVehicleCheckboxes, 1000); // Überprüfung alle 1 Sekunde (kann angepasst werden)
-
-
-
 
 // Funktion zum Auslesen des aktuellen Fahrzeugstatus von der Detailseite
 function getVehicleStatus(vehicleID) {
@@ -76,7 +78,6 @@ function getVehicleStatus(vehicleID) {
       const fmsStatusElement = doc.querySelector('.building_list_fms');
       if (fmsStatusElement) {
         const fmsStatus = fmsStatusElement.innerText;
-//        console.log(`Fahrzeug ${vehicleID}: Aktueller FMS-Status: ${fmsStatus}`);
         updateVehicleStatus(vehicleID, fmsStatus);
       }
     });
@@ -84,23 +85,28 @@ function getVehicleStatus(vehicleID) {
 
 // Funktion zum Aktualisieren des Fahrzeugstatus und Ausführen der Aktionen
 function updateVehicleStatus(vehicleID, fmsStatus) {
-  if (vehicleID === fahrzeug1ID) {
-    if (fmsStatus === '2') {
-      fahrzeug1Status = '2';
-      fahrzeug2Status = '2';
-    } else if (fmsStatus === '3') {
-      fahrzeug1Status = '3';
-      fahrzeug2Status = '2';
+  fahrzeugStatus[vehicleID] = fmsStatus;
+
+  fahrzeugPaare.forEach(fahrzeugPaar => {
+    const { fahrzeug1ID, fahrzeug2ID } = fahrzeugPaar;
+    if (vehicleID === fahrzeug1ID) {
+      if (fmsStatus === '2') {
+        fahrzeugStatus[fahrzeug1ID] = '2';
+        fahrzeugStatus[fahrzeug2ID] = '2';
+      } else if (fmsStatus === '3') {
+        fahrzeugStatus[fahrzeug1ID] = '3';
+        fahrzeugStatus[fahrzeug2ID] = '2';
+      }
+    } else if (vehicleID === fahrzeug2ID) {
+      if (fmsStatus === '2') {
+        fahrzeugStatus[fahrzeug1ID] = '2';
+        fahrzeugStatus[fahrzeug2ID] = '2';
+      } else if (fmsStatus === '3') {
+        fahrzeugStatus[fahrzeug1ID] = '2';
+        fahrzeugStatus[fahrzeug2ID] = '3';
+      }
     }
-  } else if (vehicleID === fahrzeug2ID) {
-    if (fmsStatus === '2') {
-      fahrzeug1Status = '2';
-      fahrzeug2Status = '2';
-    } else if (fmsStatus === '3') {
-      fahrzeug1Status = '2';
-      fahrzeug2Status = '3';
-    }
-  }
+  });
 }
 
 // Funktion zum Überprüfen und Steuern der Fahrzeuge
@@ -118,33 +124,34 @@ function checkAndControlVehicles() {
     const vehicleID = vehicleIDElement.getAttribute('vehicle_id');
     const fmsStatus = message.querySelector('span.building_list_fms').innerText;
 
-//    console.log(`Sprechwunsch von Fahrzeug ${vehicleID}: FMS-Status ${fmsStatus}`);
-
-    if (vehicleID === fahrzeug1ID) {
-      if (fmsStatus === '3' && fahrzeug1Status !== '3') {
-//        console.log(`Sprechwunsch von Fahrzeug ${vehicleID}: Aktion für FMS-Status ${fmsStatus}`);
-        handleFMSStatus(fahrzeug2ID, '6');
-        fahrzeug1Status = '3';
-        fahrzeug2Status = '2';
-      } else if (fmsStatus === '2' && fahrzeug1Status !== '2') {
-//        console.log(`Sprechwunsch von Fahrzeug ${vehicleID}: Aktion für FMS-Status ${fmsStatus}`);
-        handleFMSStatus(fahrzeug2ID, '2');
-        fahrzeug1Status = '2';
-        fahrzeug2Status = '2';
-      }
-    } else if (vehicleID === fahrzeug2ID) {
-      if (fmsStatus === '3' && fahrzeug2Status !== '3') {
-//        console.log(`Sprechwunsch von Fahrzeug ${vehicleID}: Aktion für FMS-Status ${fmsStatus}`);
-        handleFMSStatus(fahrzeug1ID, '6');
-        fahrzeug1Status = '2';
-        fahrzeug2Status = '3';
-      } else if (fmsStatus === '2' && fahrzeug2Status !== '2') {
-//        console.log(`Sprechwunsch von Fahrzeug ${vehicleID}: Aktion für FMS-Status ${fmsStatus}`);
-        handleFMSStatus(fahrzeug1ID, '2');
-        fahrzeug1Status = '2';
-        fahrzeug2Status = '2';
-      }
+    if (fahrzeugStatus[vehicleID] === fmsStatus) {
+      return; // Keine Änderung des FMS-Status, keine Aktion erforderlich
     }
+
+    fahrzeugPaare.forEach(fahrzeugPaar => {
+      const { fahrzeug1ID, fahrzeug2ID } = fahrzeugPaar;
+      if (vehicleID === fahrzeug1ID) {
+        if (fmsStatus === '3' && fahrzeugStatus[fahrzeug1ID] !== '3') {
+          handleFMSStatus(fahrzeug2ID, '6');
+          fahrzeugStatus[fahrzeug1ID] = '3';
+          fahrzeugStatus[fahrzeug2ID] = '2';
+        } else if (fmsStatus === '2' && fahrzeugStatus[fahrzeug1ID] !== '2') {
+          handleFMSStatus(fahrzeug2ID, '2');
+          fahrzeugStatus[fahrzeug1ID] = '2';
+          fahrzeugStatus[fahrzeug2ID] = '2';
+        }
+      } else if (vehicleID === fahrzeug2ID) {
+        if (fmsStatus === '3' && fahrzeugStatus[fahrzeug2ID] !== '3') {
+          handleFMSStatus(fahrzeug1ID, '6');
+          fahrzeugStatus[fahrzeug1ID] = '2';
+          fahrzeugStatus[fahrzeug2ID] = '3';
+        } else if (fmsStatus === '2' && fahrzeugStatus[fahrzeug2ID] !== '2') {
+          handleFMSStatus(fahrzeug1ID, '2');
+          fahrzeugStatus[fahrzeug1ID] = '2';
+          fahrzeugStatus[fahrzeug2ID] = '2';
+        }
+      }
+    });
   });
 }
 
@@ -156,8 +163,11 @@ const observer = new MutationObserver(() => {
 observer.observe(document.body, { childList: true, subtree: true });
 
 // Auslesen des aktuellen Fahrzeugstatus beim Seitenladen
-getVehicleStatus(fahrzeug1ID);
-getVehicleStatus(fahrzeug2ID);
+fahrzeugPaare.forEach(fahrzeugPaar => {
+  const { fahrzeug1ID, fahrzeug2ID } = fahrzeugPaar;
+  getVehicleStatus(fahrzeug1ID);
+  getVehicleStatus(fahrzeug2ID);
+});
 
 // Initiale Überprüfung der Fahrzeuge
 checkAndControlVehicles();
